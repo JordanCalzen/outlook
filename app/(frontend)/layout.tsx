713fd58fe7901a11703e82f4d56fa22f0e719c6d
JordanCalzen <jordancarlzencare@gmail.com>
2025-02-18 18:19:27 +0300

    redirect successful

--- app/(frontend)/layout.tsx
+++ app/(frontend)/layout.tsx
@@ -1,8 +1,18 @@
 import MainSidebar from "@/components/main-sidebar";
 import { Header } from "@/components/top-navbar";
+import { getSession } from "@/lib/dal";
+import { redirect } from "next/navigation";
 import React, { ReactNode } from "react";
 
-export default function FrontLayout({ children }: { children: ReactNode }) {
+export default async function FrontLayout({
+	children,
+}: {
+	children: ReactNode;
+}) {
+	const getTheSession = await getSession();
+	if (!getTheSession) {
+		return redirect("/authform");
+	}
 	return (
 		<div>
 			<MainSidebar />
